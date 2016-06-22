902a5c5689d574ef9de2171b18b673d0dae57afd
Dieter Deyke <dieter.deyke@gmail.com>
2016-06-22 19:39:00 +0200

    VLC supports .aac audio files

--- bongo.el
+++ bongo.el
@@ -5767,6 +5767,7 @@ These will come at the end or right before the file name, if any."
   ;; matches that of some potentially supported format.
   :matcher '((local-file "file:" "http:" "ftp:")
              "669"
+             "aac"
              "asf"
              "au"
              "avi"
